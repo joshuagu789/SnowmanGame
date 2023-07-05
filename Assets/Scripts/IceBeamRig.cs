9fba2c4b9cbec98bc10c3a1987f6da0b73a89920
joshuagu789 <joshuagu789@gmail.com>
2023-07-05 19:55:22 -0400

    Removed old off-centered assets & improved target lock

--- Assets/Scripts/IceBeamRig.cs
+++ Assets/Scripts/IceBeamRig.cs
@@ -6,12 +6,8 @@ using Cinemachine;
 public class IceBeamRig : MonoBehaviour
 {
     public Animator animator;
-    //public CinemachineVirtualCamera firstPersonCam;
-    //public Transform targetLock;
 
     bool toggleActive = false;
-    bool isLockedOn = false;
-    //public float range = 100f;
 
     // Start is called before the first frame update
     void Start()
@@ -22,29 +18,8 @@ public class IceBeamRig : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //CheckLockOn();
         // Includes actual firing of ice beam 
         CheckWeaponStates();
-    }
-
-    void CheckLockOn()
-    {
-         /*                   // Right click
-        if (Input.GetButtonDown("Fire2") && firstPersonCam.enabled)
-        {
-            // Locking on target if it's in range
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-
-            // If the has hit an enemy
-            if (Physics.Raycast(ray, out hit, range) && hit.collider.gameObject.CompareTag("Enemy"))
-            {
-                Debug.Log(hit.collider.gameObject);
-                var target = Instantiate(targetLock, hit.collider.transform.position, hit.collider.transform.localRotation, hit.collider.transform);
-            }
-            
-        }
-         */
     }
 
     void CheckWeaponStates()
--- Assets/Scripts/Snowmen/Player/PlayerTargeting.cs
+++ Assets/Scripts/Snowmen/Player/PlayerTargeting.cs
@@ -6,8 +6,13 @@ using UnityEngine;
 public class PlayerTargeting : MonoBehaviour
 {
     public CinemachineVirtualCamera firstPersonCam;
-    public float range = 100f;
     public Transform targetLock;
+
+    public float range = 100f;
+    bool isLockedOn = false;
+    int numLocks = 0;
+
+    Transform target;
 
     // Start is called before the first frame update
     void Start()
@@ -18,18 +23,27 @@ public class PlayerTargeting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CheckLockState();
+    }
+
+    void CheckLockState()
+    {
         if (Input.GetButtonDown("Fire2") && firstPersonCam.enabled)
         {
             // Locking on target if it's in range
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
-            // If the has hit an enemy
+            // If the ray has hit an enemy within range
             if (Physics.Raycast(ray, out hit, range) && hit.collider.gameObject.CompareTag("Enemy"))
             {
-                Debug.Log(hit.collider.gameObject);
-                var target = Instantiate(targetLock, hit.collider.transform.position, hit.collider.transform.localRotation, hit.collider.transform);
+                if (isLockedOn)
+                {
+                    Destroy(target.gameObject);
+                }
+                target = Instantiate(targetLock, hit.collider.transform.position, hit.collider.transform.localRotation, hit.collider.transform);
                 target.localScale = hit.collider.transform.localScale;
+                isLockedOn = true;
             }
 
         }
